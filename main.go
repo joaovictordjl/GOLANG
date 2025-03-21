146a366f81d96599d762a3c0590e16872e4f1dda
João Victor <victorjhon779@gmail.com>
2025-03-21 20:46:49 -0300

    Sobre pacotes

--- main.go
+++ main.go
@@ -7,3 +7,21 @@ func main ()  {
 }  
 
 //PRINTLN IMPRIME E PULA UMA LINHA
+
+//Package main- executará a função main antes de tudo, ao dar run no programa
+
+/*
+	PACOTES:
+	
+	São conjuntos de funções. Um pacote, tem várias funções.
+		-> fmt é um pacote e usa a função Println
+	
+	Para importar um pacote é simples:
+		-> import "nomeDoPacote"
+			Caso tenha mais de um pacote para importar, faça assim:
+				-> import (
+					"pacote1"
+					"pacote2"
+					"pacote3"
+				)
+*/
