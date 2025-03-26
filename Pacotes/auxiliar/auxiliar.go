ebc26ef52684f0abfc912ec39f06468b1a9d44d4
João Victor <victorjhon779@gmail.com>
2025-03-25 21:47:21 -0300

    Aprendendo sobre pacotes e importações de pacotes externos.

---
+++ Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+//A função escrever, registra uma mensagem na tela
+func Escrever() {
+	fmt.Println("Escrevendo do pacote auxiliar")
+}
---
+++ Pacotes/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"modulo/auxiliar"
+	"fmt"
+	"github.com/badoux/checkmail"
+)
+
+func main() {
+	fmt.Println("Escrevendo do arquivo main, textando o mod")
+	fmt.Println("teste")
+
+	auxiliar.Escrever()
+
+	checkmail.ValidateFormat("victorjhon779@gmail.com")
+}
+
+//Pacotes são conjuntos de funções ou arquivos de uma pasta
+
+/* 
+Para usar mais de um pacote em um projeto, utiliza-se o módulo
+	Para criar um módulo:
+		go mod init modulo
+	Modulo é onde os pacotes ficam armazenados
+
+
+Após criar um módulo e escrever algum código, escreva no terminal:
+	go build
+		Isso irá gerar um arquivo binário, que compila todos os arquivos do seu projeto em um só, que seria esse binário.
+			É possivel executar seu projeto rodando apenas esse arquivo binário, da seguinte forma:
+				./modulo
+
+
+Cada pacote precisa estar em uma pasta diferente, ou seja, sempre que for criar um pacote é necessário criar uma pasta para ele.				
+*/
